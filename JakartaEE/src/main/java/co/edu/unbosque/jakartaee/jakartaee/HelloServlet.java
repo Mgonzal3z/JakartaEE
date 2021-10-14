2e136750ad7db4f604ccbca2cf98815a87dfdbc1
crisvac10 <82240074+crisvac10@users.noreply.github.com>
2021-10-13 20:05:48 -0500

    Update HelloServlet.java

--- JakartaEE/src/main/java/co/edu/unbosque/jakartaee/jakartaee/HelloServlet.java
+++ JakartaEE/src/main/java/co/edu/unbosque/jakartaee/jakartaee/HelloServlet.java
@@ -3,6 +3,7 @@
 import java.io.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import javax.swing.plaf.synth.SynthTextAreaUI;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
@@ -14,14 +15,48 @@ public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
-        // Hello
         PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
+        Cookie cookie = new Cookie("Propietario","crisvac");
+        response.addCookie(cookie);
+        // Hello
 
+            out.println("<html><body>");
+            out.println("<meta http-equiv ='refresh'  content = '10;  URL= form.html'>");
+            out.println("</head> <body>");
+            out.println("<h1>" + message + "</h1>");
+            out.println("</body></html>");
+          out.println("<html><body>");
+            out.println("<meta http-equiv ='refresh'  content = '10;  URL= form2.html'>");
+            out.println("</head> <body>");
+            out.println("<h1>" + message + "</h1>");
+            out.println("</body></html>");
+
+
+
+
+
+
+
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        String username = request.getParameter("user");
+        String password = request.getParameter("password");
+        System.out.println("user:" + username);
+        System.out.println();
+        if (username.equals("crvacar") && password.equals("crvacar2020")) {
+            // Hello
+            PrintWriter out = response.getWriter();
+            out.println("<html><body>");
+            out.println("<h1> Hello " + username + "</h1>");
+            out.println("</body></html>");
+
+
+        } else {
+            response.sendError(401);
+        }
+    }
     public void destroy() {
     }
 }
